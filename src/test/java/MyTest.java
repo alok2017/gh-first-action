fb2edadf609ccdef1b6080ba1a48da973be250e5
alok2017 <31183475+alok2017@users.noreply.github.com>
2025-05-18 06:36:40 +0530

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,1 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class MyTest {
+    @Test
+    void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
