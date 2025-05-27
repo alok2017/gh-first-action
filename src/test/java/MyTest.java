c9097351839e7ee24b1e4e5ca732846e866e6e96
alok2017 <31183475+alok2017@users.noreply.github.com>
2025-05-27 08:50:38 +0530

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MyTest {
     @Test
     void testAddition() {
-        int sum = 2 + 3;
+        int sum = 2 + 3
         assertEquals(5, sum);
         System.out.println("✅ Test passed: Addition works correctly!");
 
